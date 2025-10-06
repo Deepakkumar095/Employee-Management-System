82c9069982e27b01a45b239b2a08c54b6f37e698
Deepakkumar095 <141207306+Deepakkumar095@users.noreply.github.com>
2025-10-07 01:42:13 +0530

    Update Navbar.jsx

--- my-project/src/component/Navbar.jsx
+++ my-project/src/component/Navbar.jsx
@@ -7,17 +7,7 @@ const Navbar = ({ isAuthenticated }) => {
       <h1 className="text-3xl font-bold text-green-500">
         EM Service Incharge
       </h1>
-      <div className="space-x-4 ml-auto">
-        <Link className="hover:text-blue-400 text-white" to="/">Home</Link>
-        {isAuthenticated && (
-          <Link className="hover:text-blue-400 text-white" to="/profile">Profile</Link>
-        )}
-        {!isAuthenticated ? (
-          <Link className="hover:text-blue-400 text-white" to="/login">Login</Link>
-        ) : (
-          <Link className="hover:text-blue-400 text-white" to="/profile">Logout</Link>
-        )}
-      </div>
+      
     </div>
   );
 };
